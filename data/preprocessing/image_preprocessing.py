5ccbf9163c6e73261090b548f3857a51bda89caa
alexchung <yonganzhong@outlook.com>
2020-08-18 14:36:29 +0800

    add image preprocessing module

---
+++ data/preprocessing/image_preprocessing.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------
+# @ File       : image_preprocessing.py
+# @ Description:  
+# @ Author     : Alex Chung
+# @ Contact    : yonganzhong@outlook.com
+# @ License    : Copyright (c) 2017-2018
+# @ Time       : 2020/8/18 下午2:34
+# @ Software   : PyCharm
+#-------------------------------------------------------
+import cv2 as cv
+import numpy as np
+
+
+def image_resize_padding(image, target_size, gt_boxes=None):
+
+    image = cv.cvtColor(image, cv.COLOR_BGR2RGB).astype(np.float32)
+
+    ih, iw    = target_size
+    h,  w, _  = image.shape
+
+    scale = min(iw/w, ih/h)
+    nw, nh  = int(scale * w), int(scale * h)
+    image_resized = cv.resize(image, (nw, nh))
+
+    image_paded = np.full(shape=[ih, iw, 3], fill_value=128.0)
+    dw, dh = (iw - nw) // 2, (ih-nh) // 2
+    image_paded[dh:nh+dh, dw:nw+dw, :] = image_resized
+    image_paded = image_paded / 255.
+
+    if gt_boxes is None:
+        return image_paded
+
+    else:
+        gt_boxes[:, [0, 2]] = gt_boxes[:, [0, 2]] * scale + dw
+        gt_boxes[:, [1, 3]] = gt_boxes[:, [1, 3]] * scale + dh
+        return image_paded, gt_boxes
