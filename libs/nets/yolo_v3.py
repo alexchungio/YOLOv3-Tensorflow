2a804de0034127188a40486b9da234bb802aed77
alexchung <yonganzhong@outlook.com>
2020-08-24 14:42:45 +0800

    add cosine_decay_with_warmup function

--- libs/nets/yolo_v3.py
+++ libs/nets/yolo_v3.py
@@ -341,3 +341,54 @@ class YOLOV3(object):
 
         return restorer, checkpoint_path
 
+    def cosine_decay_with_warmup(self, global_step,
+                                 learning_rate_base,
+                                 total_decay_steps,
+                                 learning_rate_end=0.0,
+                                 warmup_learning_rate=0.0,
+                                 warmup_steps=0,
+                                 hold_base_rate_steps=0):
+        """Cosine decay schedule with warm up period.
+
+        """
+        if total_decay_steps < warmup_steps:
+            raise ValueError('total_steps must be larger or equal to '
+                             'warmup_steps.')
+
+        def eager_decay_rate():
+            """Callable to compute the learning rate."""
+            # reference cosine_decay
+            # cosine_decay = 0.5 * (1 + cos(pi * global_step / decay_steps))
+            # decayed = (1 - alpha) * cosine_decay + alpha
+            # decayed_learning_rate = learning_rate * decayed
+            # where alpha = 0
+            # global_step = global_step - (warmup_steps + hold_base_rate_steps)
+            # decay_step = total_steps - (warmup_steps + hold_base_rate_steps)
+            # learning_rate = 0.5 * learning_rate_base * (1 + tf.cos(
+            #     np.pi *(tf.cast(global_step, tf.float32) - warmup_steps - hold_base_rate_steps
+            #     ) / float(total_decay_steps - warmup_steps - hold_base_rate_steps)))
+            learning_rate = tf.train.cosine_decay(learning_rate=learning_rate_base,
+                                                  decay_steps=total_decay_steps - warmup_steps - hold_base_rate_steps,
+                                                  global_step=global_step - warmup_steps - hold_base_rate_steps,
+                                                  alpha=learning_rate_end)
+            if hold_base_rate_steps > 0:
+                learning_rate = tf.where(
+                    global_step > warmup_steps + hold_base_rate_steps,
+                    learning_rate, learning_rate_base)
+            if warmup_steps > 0:
+                if learning_rate_base < warmup_learning_rate:
+                    raise ValueError('learning_rate_base must be larger or equal to '
+                                     'warmup_learning_rate.')
+                slope = (learning_rate_base - warmup_learning_rate) / warmup_steps
+                warmup_rate = slope * tf.cast(global_step,
+                                              tf.float32) + warmup_learning_rate
+                learning_rate = tf.where(global_step < warmup_steps, warmup_rate,
+                                         learning_rate)
+            return tf.where(global_step > total_decay_steps, learning_rate_end, learning_rate,
+                            name='learning_rate')
+
+        if tf.executing_eagerly():
+            return eager_decay_rate
+        else:
+            return eager_decay_rate()
+
