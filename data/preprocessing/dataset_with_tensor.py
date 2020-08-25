7c516f1d92caeb0b1077f7befbb4230da5f1712f
alexchung <yonganzhong@outlook.com>
2020-08-25 14:58:45 +0800

    add dataset_with_tensor

---
+++ data/preprocessing/dataset_with_tensor.py
@@ -0,0 +1,300 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------
+# @ File       : dataset_with_tensor.py
+# @ Description:  
+# @ Author     : Alex Chung
+# @ Contact    : yonganzhong@outlook.com
+# @ License    : Copyright (c) 2017-2018
+# @ Time       : 2020/8/25 上午9:45
+# @ Software   : PyCharm
+#-------------------------------------------------------
+import os
+import cv2
+import random
+import numpy as np
+import tensorflow as tf
+from data.preprocessing.yolo_preprocessing import image_resize_padding
+from libs.configs import cfgs
+import utils.tools as tools
+
+
+class Dataset(object):
+    """implement Dataset here"""
+    def __init__(self, is_training=True):
+
+        self.is_training = is_training
+        self.annot_path  = cfgs.TRAIN_ANNOT_PATH if self.is_training else cfgs.TEST_ANNOT_PATH
+        self.input_sizes = cfgs.TRAIN_INPUT_SIZE if self.is_training  else cfgs.TRAIN_INPUT_SIZE
+        self.batch_size  = cfgs.TRAIN_BATCH_SIZE if self.is_training else cfgs.TEST_BATCH_SIZE
+
+        self.train_input_sizes = cfgs.TRAIN_INPUT_SIZE
+        self.strides = np.array(cfgs.STRIDES)
+        self.classes = tools.read_class_names(cfgs.CLASSES)
+        self.num_classes = len(self.classes)
+        self.anchors = np.array(tools.get_anchors(cfgs.ANCHORS))
+        self.anchor_per_scale = cfgs.ANCHOR_PER_SCALE
+        self.max_bbox_per_scale = 150
+
+        self.annotations = self.load_annotations()
+        self.num_samples = len(self.annotations)
+        self.num_batchs = int(np.ceil(self.num_samples / self.batch_size))
+        self.batch_count = 0
+
+    def load_annotations(self):
+        with open(self.annot_path, 'r') as f:
+            txt = f.readlines()
+            annotations = [line.strip() for line in txt if len(line.strip().split()[1:]) != 0]
+        np.random.shuffle(annotations)
+        return annotations
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+
+        with tf.device('/cpu:0'):
+            self.train_input_size = random.choice(self.train_input_sizes)
+            self.train_output_sizes = self.train_input_size // self.strides
+
+            num = 0
+            if self.batch_count < self.num_batchs:
+                while num < self.batch_size:
+                    index = self.batch_count * self.batch_size + num
+                    if index >= self.num_samples: index -= self.num_samples
+                    annotation = self.annotations[index]
+                    image, bboxes = self.parse_annotation(annotation)
+                    image = tf.reshape(image, shape=(self.train_input_size, self.train_input_size, 3))
+                    bboxes = tf.reshape(bboxes, shape=(-1, 5))
+                    label_sbbox, label_mbbox, label_lbbox, sbboxes, mbboxes, lbboxes = tf.py_func(self.preprocess_true_boxes,
+                                                                                                  inp=[bboxes],
+                                                                                                  Tout=[tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32])
+
+                    label_sbbox = tf.reshape(label_sbbox, shape=(self.train_output_sizes[0], self.train_output_sizes[0],
+                                                                 self.anchor_per_scale, 5 + self.num_classes))
+                    label_mbbox = tf.reshape(label_mbbox, shape=(self.train_output_sizes[1], self.train_output_sizes[1],
+                                                                 self.anchor_per_scale, 5 + self.num_classes))
+                    label_lbbox = tf.reshape(label_lbbox, shape=(self.train_output_sizes[2], self.train_output_sizes[2],
+                                                                 self.anchor_per_scale, 5 + self.num_classes))
+                    sbboxes = tf.reshape(sbboxes, shape=(self.max_bbox_per_scale, 4))
+                    mbboxes = tf.reshape(mbboxes, shape=(self.max_bbox_per_scale, 4))
+                    lbboxes = tf.reshape(lbboxes, shape=(self.max_bbox_per_scale, 4))
+                    # label_sbbox, label_mbbox, label_lbbox, sbboxes, mbboxes, lbboxes = self.preprocess_true_boxes(bboxes)
+                    if num == 0:
+                        batch_image = tf.expand_dims(image, axis=0)
+                        batch_label_sbbox = tf.expand_dims(label_sbbox, 0)
+                        batch_label_mbbox = tf.expand_dims(label_mbbox, 0)
+                        batch_label_lbbox = tf.expand_dims(label_lbbox, 0)
+                        batch_sbboxes = tf.expand_dims(sbboxes, 0)
+                        batch_mbboxes = tf.expand_dims(mbboxes, 0)
+                        batch_lbboxes = tf.expand_dims(lbboxes, 0)
+                    else:
+                        batch_image = tf.concat((batch_image, tf.expand_dims(image, axis=0)), axis=0)
+                        batch_label_sbbox = tf.concat((batch_label_sbbox, tf.expand_dims(label_sbbox, 0)), axis=0)
+                        batch_label_mbbox = tf.concat((batch_label_mbbox, tf.expand_dims(label_mbbox, 0)), axis=0)
+                        batch_label_lbbox = tf.concat((batch_label_lbbox, tf.expand_dims(label_lbbox, 0)), axis=0)
+                        batch_sbboxes = tf.concat((batch_sbboxes, tf.expand_dims(sbboxes, 0)), axis=0)
+                        batch_mbboxes = tf.concat((batch_mbboxes, tf.expand_dims(mbboxes, 0)), axis=0)
+                        batch_lbboxes = tf.concat((batch_lbboxes, tf.expand_dims(lbboxes, 0)), axis=0)
+                    num += 1
+
+                self.batch_count += 1
+                return batch_image, batch_label_sbbox, batch_label_mbbox, batch_label_lbbox, \
+                       batch_sbboxes, batch_mbboxes, batch_lbboxes
+
+            else:
+                self.batch_count = 0
+                np.random.shuffle(self.annotations)
+                raise StopIteration
+
+    def random_horizontal_flip(self, image, bboxes):
+
+        if random.random() < 0.5:
+            _, w, _ = image.shape
+            image = image[:, ::-1, :]
+            bboxes[:, [0,2]] = w - bboxes[:, [2,0]]
+
+        return image, bboxes
+
+    def random_crop(self, image, bboxes):
+
+        if random.random() < 0.5:
+            h, w, _ = image.shape
+            max_bbox = np.concatenate([np.min(bboxes[:, 0:2], axis=0), np.max(bboxes[:, 2:4], axis=0)], axis=-1)
+
+            max_l_trans = max_bbox[0]
+            max_u_trans = max_bbox[1]
+            max_r_trans = w - max_bbox[2]
+            max_d_trans = h - max_bbox[3]
+
+            crop_xmin = max(0, int(max_bbox[0] - random.uniform(0, max_l_trans)))
+            crop_ymin = max(0, int(max_bbox[1] - random.uniform(0, max_u_trans)))
+            crop_xmax = max(w, int(max_bbox[2] + random.uniform(0, max_r_trans)))
+            crop_ymax = max(h, int(max_bbox[3] + random.uniform(0, max_d_trans)))
+
+            image = image[crop_ymin : crop_ymax, crop_xmin : crop_xmax]
+
+            bboxes[:, [0, 2]] = bboxes[:, [0, 2]] - crop_xmin
+            bboxes[:, [1, 3]] = bboxes[:, [1, 3]] - crop_ymin
+
+        return image, bboxes
+
+    def random_translate(self, image, bboxes):
+
+        if random.random() < 0.5:
+            h, w, _ = image.shape
+            max_bbox = np.concatenate([np.min(bboxes[:, 0:2], axis=0), np.max(bboxes[:, 2:4], axis=0)], axis=-1)
+
+            max_l_trans = max_bbox[0]
+            max_u_trans = max_bbox[1]
+            max_r_trans = w - max_bbox[2]
+            max_d_trans = h - max_bbox[3]
+
+            tx = random.uniform(-(max_l_trans - 1), (max_r_trans - 1))
+            ty = random.uniform(-(max_u_trans - 1), (max_d_trans - 1))
+
+            M = np.array([[1, 0, tx], [0, 1, ty]])
+            image = cv2.warpAffine(image, M, (w, h))
+
+            bboxes[:, [0, 2]] = bboxes[:, [0, 2]] + tx
+            bboxes[:, [1, 3]] = bboxes[:, [1, 3]] + ty
+
+        return image, bboxes
+
+    def parse_annotation(self, annotation):
+
+        line = annotation.split()
+        image_path = line[1]
+        if not os.path.exists(image_path):
+            raise KeyError("%s does not exist ... " %image_path)
+        image = np.array(cv2.imread(image_path))
+        bboxes = np.array([list(map(lambda x: int(float(x)), box.split(','))) for box in line[4:]])
+
+        image, bboxes = tf.convert_to_tensor(image), tf.convert_to_tensor(bboxes)
+
+        image, bboxes = tf.py_func(self.image_processing, inp=[image, bboxes], Tout=[tf.float32, tf.float32])
+
+        return image, bboxes
+
+    def image_processing(self, image, bboxes):
+        if self.is_training:
+            image, bboxes = self.random_horizontal_flip(np.copy(image), np.copy(bboxes))
+            image, bboxes = self.random_crop(np.copy(image), np.copy(bboxes))
+            image, bboxes = self.random_translate(np.copy(image), np.copy(bboxes))
+
+        image, bboxes = image_resize_padding(np.copy(image), [self.train_input_size, self.train_input_size], np.copy(bboxes))
+        return image, bboxes
+
+    def bbox_iou(self, boxes1, boxes2):
+        """
+        compute bboxes iou
+        :param boxes1:
+        :param boxes2:
+        :return:
+        """
+        boxes1 = np.array(boxes1)
+        boxes2 = np.array(boxes2)
+
+        boxes1_area = boxes1[..., 2] * boxes1[..., 3]
+        boxes2_area = boxes2[..., 2] * boxes2[..., 3]
+
+        boxes1 = np.concatenate([boxes1[..., :2] - boxes1[..., 2:] * 0.5,
+                                boxes1[..., :2] + boxes1[..., 2:] * 0.5], axis=-1)
+        boxes2 = np.concatenate([boxes2[..., :2] - boxes2[..., 2:] * 0.5,
+                                boxes2[..., :2] + boxes2[..., 2:] * 0.5], axis=-1)
+
+        left_up = np.maximum(boxes1[..., :2], boxes2[..., :2])
+        right_down = np.minimum(boxes1[..., 2:], boxes2[..., 2:])
+
+        inter_section = np.maximum(right_down - left_up, 0.0)
+        inter_area = inter_section[..., 0] * inter_section[..., 1]
+        union_area = boxes1_area + boxes2_area - inter_area
+
+        return inter_area / union_area
+
+    def preprocess_true_boxes(self, bboxes):
+
+        # small_label, middle_label, larger_label
+        label = [np.zeros((self.train_output_sizes[i], self.train_output_sizes[i], self.anchor_per_scale,
+                           5 + self.num_classes)) for i in range(3)]
+        # small_anchor, middle_scale, large_scale
+        bboxes_xywh = [np.zeros((self.max_bbox_per_scale, 4)) for _ in range(3)]
+        bbox_count = np.zeros((3,))
+
+        for bbox in bboxes:
+            bbox_coor = bbox[:4]
+            bbox_class_ind = bbox[4]
+
+            # one_hot_label
+            onehot = np.zeros(self.num_classes, dtype=np.float)
+            onehot[bbox_class_ind] = 1.0
+
+            # label_smooth
+            uniform_distribution = np.full(self.num_classes, 1.0 / self.num_classes)
+            delta = 0.01
+            smooth_onehot = onehot * (1 - delta) + delta * uniform_distribution
+
+            # (x_min, y_min, x_max, y_max) -> (x, y, w, h)
+            bbox_xywh = np.concatenate([(bbox_coor[2:] + bbox_coor[:2]) * 0.5, bbox_coor[2:] - bbox_coor[:2]], axis=-1)
+            # get bbox of three feature(small, middle, large)
+            bbox_xywh_scaled = 1.0 * bbox_xywh[np.newaxis, :] / self.strides[:, np.newaxis]
+
+            iou = []
+            exist_positive = False
+
+            # iterate small middle and large
+            for i in range(3):
+                # get anchor bbox of three scale
+                anchors_xywh = np.zeros((self.anchor_per_scale, 4))
+                anchors_xywh[:, 0:2] = np.floor(bbox_xywh_scaled[i, 0:2]).astype(np.int32) + 0.5
+                anchors_xywh[:, 2:4] = self.anchors[i]
+
+                # computer one bbox iou at three scale
+                iou_scale = self.bbox_iou(bbox_xywh_scaled[i][np.newaxis, :], anchors_xywh)
+                iou.append(iou_scale)
+                iou_mask = iou_scale > 0.3
+
+                if np.any(iou_mask):
+                    # save bbox box, conf and classify witch iou value over threshold
+                    xind, yind = np.floor(bbox_xywh_scaled[i, 0:2]).astype(np.int32)
+
+                    label[i][yind, xind, iou_mask, :] = 0
+                    label[i][yind, xind, iou_mask, 0:4] = bbox_xywh
+                    label[i][yind, xind, iou_mask, 4:5] = 1.0
+                    label[i][yind, xind, iou_mask, 5:] = smooth_onehot
+
+                    # save bbox box
+                    bbox_ind = int(bbox_count[i] % self.max_bbox_per_scale)
+                    bboxes_xywh[i][bbox_ind, :4] = bbox_xywh
+                    bbox_count[i] += 1
+
+                    exist_positive = True
+
+            if not exist_positive:
+                best_anchor_ind = np.argmax(np.array(iou).reshape(-1), axis=-1)
+                best_detect = int(best_anchor_ind / self.anchor_per_scale)
+                best_anchor = int(best_anchor_ind % self.anchor_per_scale)
+                xind, yind = np.floor(bbox_xywh_scaled[best_detect, 0:2]).astype(np.int32)
+
+                label[best_detect][yind, xind, best_anchor, :] = 0
+                label[best_detect][yind, xind, best_anchor, 0:4] = bbox_xywh
+                label[best_detect][yind, xind, best_anchor, 4:5] = 1.0
+                label[best_detect][yind, xind, best_anchor, 5:] = smooth_onehot
+
+                bbox_ind = int(bbox_count[best_detect] % self.max_bbox_per_scale)
+                bboxes_xywh[best_detect][bbox_ind, :4] = bbox_xywh
+                bbox_count[best_detect] += 1
+        label_sbbox, label_mbbox, label_lbbox = label
+        sbboxes, mbboxes, lbboxes = bboxes_xywh
+        return label_sbbox, label_mbbox, label_lbbox, sbboxes, mbboxes, lbboxes
+
+    def __len__(self):
+        return self.num_batchs
+
+if __name__ == "__main__":
+
+    dataset = Dataset(is_training=True)
+
+    for data in dataset:
+        print(data[0].shape, data[1].shape, data[2].shape, data[3].shape, data[4].shape, data[5].shape, data[6].shape)
+        # print(data[0].shape, data[1].shape)
